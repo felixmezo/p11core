0cd0b7dd4801a6c2aafce9db70f58ed34d3dbbe2
Felix Mezo <felix.mezog@alumnos.upm.es>
2016-05-12 12:01:00 +0200

    Tema 16: Borrar preguntas

---
+++ models/index.js
@@ -0,0 +1,57 @@
+
+var path = require('path');
+
+// Cargar ORM
+var Sequelize = require('sequelize');
+
+// Usar BBDD SQLite:
+    // DATABASE_URL = "sqlite:///"
+    // DATABASE_STORAGE = "quiz.sqlite"
+// Usar BBDD Postgres:
+ //  DATABASE_URL = "postgres://hcyqsozfgpcltg:WQ6pBdA7aeq880nryX8Ov2UxXe@ec2-54-225-123-119.compute-1.amazonaws.com:5432/dauc7s2vbb9oan"
+
+var url, storage
+
+if (!process.env.DATABASE_URL) {
+    url = "sqlite:///";
+    storage = "quiz.sqlite";
+}else{
+    url = process.env.DATABASE_URL;
+    storage = process.env.DATABASE_STORAGE || "";
+}
+var sequelize = new Sequelize(url, 
+                              { storage: storage,
+                                omitNull: true 
+                              });
+
+
+// Importar la definicion de la tabla Quiz de quiz.js
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+
+// sequelize.sync() crea e inicializa tabla de preguntas en DB
+sequelize.sync()
+    .then(function() {
+        // Ya se han creado las tablas necesarias.
+        return Quiz.count()
+                .then(function (c) {
+                    if (c === 0) {   // la tabla se inicializa solo si está vacía
+                        return Quiz.bulkCreate([ {question: 'Capital de Italia',   answer: 'Roma'},
+                                                 {question: 'Capital de Portugal', answer: 'Lisboa'},
+                                                 {question: 'Capital de Suecia', answer: 'Estocolmo'}
+                                               ])
+                                   .then(function() {
+                                        console.log('Base de datos inicializada con datos');
+                                    });
+                    }
+                });
+    })
+    .catch(function(error) {
+        console.log("Error Sincronizando las tablas de la BBDD:", error);
+        process.exit(1);
+    });
+
+
+
+exports.Quiz = Quiz; // exportar definición de tabla Quiz
+                                                
